8fc95333f6aaa84f3ea19ad25edfb71f87d8ae28
Norkys González <norkysgc@gmail.com>
2023-01-16 18:49:50 +0000

    Se modifican los enlaces para que se abran en nueva ventana.

--- src/js/app.js
+++ src/js/app.js
@@ -50,19 +50,20 @@ function render(variables = {}) {
           <h3>${variables.city ? variables.city : "Your City"}, ${
     variables.country ? variables.country : "Your Country"
   }</h3>
-          <ul class="${variables.socialMediaPosition}">
+          <ul class="${variables.socialMediaPosition}" target="_blank">
             <li><a href="https://twitter.com/ ${
               variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
               variables.github
-            }"><i class="fab fa-github"></i></a></li>
+            }" target="_blank"><i class="fab fa-github"></i></a></li>
+            
             <li><a href="https://linkedin.com/${
               variables.linkedin
-            }"><i class="fab fa-linkedin"></i></a></li>
+            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
               variables.instagram
-            }"><i class="fab fa-instagram"></i></a></li>
+            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
